e2386b27b9f3e090c7c5fc1fa12ffed34bdb9b40
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-22 13:17:36 +0530

    feat: add API endpoints for user login

---
+++ src/core/api/api-endpoints.ts
@@ -0,0 +1,11 @@
+import { environment } from "../../environments/environment.prod"
+
+const BASE_URL = environment.apiBaseUrl;
+
+export const API_ENDPOINTS = {
+
+  user: {
+    LogIn: `${BASE_URL}/auth/login`
+  }
+
+}
