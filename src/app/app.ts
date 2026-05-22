7dea05ecf0fc784ed95d90ad68f901fb8cdf018d
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-22 12:45:34 +0530

    feat: add landing page styles and import component in app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingPage } from "./pages/landing-page/landing-page";
 
 @Component({
   selector: 'app-root',
