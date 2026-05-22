019134aa036f85b6539cde64680d74dd40293854
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-22 14:20:20 +0530

    feat: add KanbanColumn component with HTML, CSS, and unit tests

---
+++ src/app/components/kanban-column/kanban-column.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban-column',
+  imports: [],
+  templateUrl: './kanban-column.html',
+  styleUrl: './kanban-column.css',
+})
+export class KanbanColumn {}
