d24f464785dab15e97016f0cccb34407ee505905
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-22 13:03:33 +0530

    feat: add route for log-in component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './pages/landing-page/landing-page';
+import { LogIn } from './pages/log-in/log-in';
 
 export const routes: Routes = [
 
   {
     path: '',
     component: LandingPage
+  },
+  {
+    path: '/auth/login',
+    component: LogIn
   }
 
 
